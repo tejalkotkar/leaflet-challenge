d808006921307d1cce1cd37cb0eeba312c6a181c
tejalkotkar <kotkar.tejal@gmail.com>
2021-01-30 20:03:42 -0600

    Added comments

--- Leaflet-Step-2/static/js/advanced.js
+++ Leaflet-Step-2/static/js/advanced.js
@@ -1,15 +1,28 @@
+// Store API endpoint as url
 var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 var plate_url = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_plates.json"
                  
-
+/*
+getRadius =>
+Function to adjust the radius of the circle.
+Takes the magnitude as the parameter and return the radius.
+*/
 getRadius=(mag)=>{
+    // If magnitude of the earthquake is 0, then return the radius as 30000
     if(mag === 0){
         return 30000;
-    } else{
+    } 
+    // If magnitude of the earthquake is not 0, then return the radius as 30000 times of magnitude
+    else{
         return mag * 30000
     }
 }
 
+/*
+getColor =>
+Function to get the color of the circle.
+Takes the depth as the parameter and return the color depending on value of depth.
+*/
 getColor=(depth)=>{
     switch(true){
         case depth >= -10 && depth < 10:
@@ -29,12 +42,16 @@ getColor=(depth)=>{
 }
 
 createFeatures=(earthquakeData, platesData)=>{
-
+    // Define a function we want to run once for each feature in the features array
+    // Give each feature a popup describing the place, magnitude and depth of the earthquake
     onEachFeature=(feature, layer)=>{
         layer.bindPopup("<h1>Place:" + feature.properties.place + "</h1> <hr> <h3>Magnitude: " + 
          feature.properties.mag + "</h3> <hr> <h3>Depth: " + feature.geometry.coordinates[2] + "</h3>");
     }
 
+    // Create a GeoJSON layer containing the features array on the earthquakeData object
+    // Run the onEachFeature function once for each piece of data in the array
+    // PointToLayer to create a circles
     var earthquakes = L.geoJSON(earthquakeData,{
         pointToLayer:(feature, latlang) => {
             return new L.circle(latlang,{
@@ -49,15 +66,16 @@ createFeatures=(earthquakeData, platesData)=>{
         onEachFeature: onEachFeature
     });
 
-    
+    // Create a GeoJSON layer containing the features array on the platesData object
     var plates = L.geoJSON(platesData,{color: "red"});
 
+    // Sending earthquakes & plates layer to the createMap function
     createMap(earthquakes, plates);
 }
 
 createMap=(earthquakes, plates)=>{
 
-    // Define tileLayers
+    // Define satellite, grayscale & outdoors tileLayers
     const satellite = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
@@ -79,19 +97,20 @@ createMap=(earthquakes, plates)=>{
         accessToken: API_KEY
     });
 
-    // Create a baseMaps object to hold the lightmap layer
+    // Create a baseMaps object to hold the tile layers
     var baseMaps = {
         Satellite: satellite,
         Grayscale: grayscale,
         Outdoors: outdoors
     };
     
-    // Create an overlayMaps object to hold the bikeStations layer
+    // Create an overlayMaps object to hold the earthquakes & plates layer
     var overlayMaps = {
         Earthquakes: earthquakes,
         TectonicsPlates: plates
     };
 
+    // Create map, giving it the satelight and earthquakes layers to display on load
     var myMap = L.map("map", {
         center: [37.09, -119.42],
         zoom: 5,
@@ -107,49 +126,47 @@ createMap=(earthquakes, plates)=>{
     });
 
     // Add in our legend
+    // define the position of the legend
     let legend = L.control({position: 'bottomright'});
+
     legend.onAdd = function (myMap) {
+        // Create HTML div element
         let div = L.DomUtil.create('div', 'info legend');
+
+        // Create labels array with the heading of the lagend as the first value
         var labels = ['&nbsp&nbsp<strong>Earthquake Depth (km)</strong>'];
-            
+        
+        // Defining the depth_categories and the depth_categories_color to get the legend text & color
         depth_categories = ['-10-10','10-30','30-50','50-70','70-90','90+'];
         depth_categories_color = [9, 11, 31, 51, 71, 100];
-    
+        
+        // Loop through the depth_categories and get the color & text
         for (let i = 0; i < depth_categories.length; i++) {
-            div.innerHTML += 
+            div.innerHTML +=
+            // Add the legend labels to the labels
             labels.push(`<i style="background:${getColor(depth_categories_color[i])}"></i>${depth_categories[i]}`);
         }
         div.innerHTML = labels.join('<br>');
+        // Return div element
         return div;
     };
+    // Add legend to map
     legend.addTo(myMap);
 }
 
-// var earthquake_data;
-// var plates_data;
-
-// init=()=>{
-//     // var earthquake_data;
-//     // var plates_data;
-//     d3.json(url, function(data){
-//         earthquake_data = data.features;
-//     });
-    
-//     d3.json(plate_url, function(platedata){
-//         //console.log(platedata.features);
-//         plates_data = platedata.features;
-//         //console.log(plates_data);
-//     });
-//     console.log(plates_data);
-// }
-
-// init()
-
-
+// Perform a GET request to the query earthquake URL
 d3.json(url, function(data){
+
+    // Once we get a response, store the data.features object to earthquake_data
     var earthquake_data = data.features;
+
+    // Perform a GET request to the query tactonic plates URL
     d3.json(plate_url, function(platedata){
+
+        // Once we get a response, store the data.features object to plates_data
         var plates_data = platedata.features;
+        
+        // send the earthquake_data & plates_data object to the createFeatures function
         createFeatures(earthquake_data, plates_data);
     });
 });
